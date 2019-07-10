3701978a8469f6749bf1f5cd8eb4847972859ee4
Guest <support@rjdm.com>
2019-07-10 09:54:03 +0100

    Initial Commit

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        Debug.Log("Butterfly Clicked");
+        gameObject.GetComponent<Animation>().enabled = false;
+    }
+}
